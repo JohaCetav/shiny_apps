1656e17a22c1383885ae3498ad96aa80cf5f7970
Johanna Salazar Ramírez <johannasalazarramirez@gmail.com>
2023-11-04 20:17:26 -0600

    Iniciar documento de la practica progamada y cargar bibliotecas y base de datos

--- Programada_1_Shiny/app.R
+++ Programada_1_Shiny/app.R
@@ -1,4 +1,27 @@
 ---
-  JOHANNA
+title: "Práctica Programada #1 Shiny"
+author: "Johanna Salazar Ramírez"
+date: "2023-11-04"
+---
 
-# Prueba Git Hub
+# Bibliotecas disponibles:
+library(shiny)
+library(datasets)
+library(shinydashboard)
+library(ggplot2)
+library(dplyr)
+library(readr)
+library(readxl)
+library(tidyr)
+library(janitor) 
+library(lubridate) 
+library(gt)
+library(leaflet)
+library(writexl)
+
+
+#Agregar Base de Datos
+datos_empleo_genero <- read_csv("Datos_empleo/datos_empleo_genero.csv")
+
+#Visualizr Base de Datos cargada
+View(datos_empleo_genero)
