f8b4b7ed3f2272ce34be669284210784334ede25
Johanna Salazar Ramírez <johannasalazarramirez@gmail.com>
2023-11-04 23:13:53 -0600

    Crear aplicación Shiny, según indicaciones

--- Programada_1_Shiny/app.R
+++ Programada_1_Shiny/app.R
@@ -1,10 +1,3 @@
----
-title: "Práctica Programada #1 Shiny"
-author: "Johanna Salazar Ramírez"
-date: "2023-11-04"
----
-
-# Bibliotecas disponibles:
 library(shiny)
 library(datasets)
 library(shinydashboard)
@@ -18,10 +11,90 @@ library(lubridate)
 library(gt)
 library(leaflet)
 library(writexl)
+library(DT)
 
 
 #Agregar Base de Datos
-datos_empleo_genero <- read_csv("Datos_empleo/datos_empleo_genero.csv")
+
+datos_empleo <- read_csv("Datos_empleo/datos_empleo_genero.csv")
 
 #Visualizr Base de Datos cargada
-View(datos_empleo_genero)
+
+View(datos_empleo)
+
+# Desarrollar unstrucciones
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Información Laboral por Género en América Latina y el Caribe", titleWidth = 810),
+  dashboardSidebar(
+    selectInput("pais", "Seleccione País", choices = unique(datos_empleo$pais_region)),
+    selectInput("genero", "Seleccione Género", choices = c("Hombres", "Mujeres")),
+    selectInput("ano_inicio", "Seleccione el Período (Múltiple)", choices = unique(datos_empleo$anyo)[!(unique(datos_empleo$anyo) %in% 1970:1999)], multiple = TRUE),
+    actionButton("actualizar", "Iniciar")
+  ),
+  dashboardBody(
+    fluidRow(
+      box(
+        title = "Gráfico de Baras Empleadores según género",
+        plotOutput("plot_empleadoras", height = "400px")
+      ),
+      box(
+        title = "Gráfico de Barras Trabajadores Informales Según Género",
+        plotOutput("plot_informal", height = "400px")
+      ),
+      box(
+        title = "Gráfico de Barras Sobre Desempleo Según Género",
+        plotOutput("plot_desempleo", height = "400px")
+      )
+    ),
+    box(
+      title = "Datos Empleo y Género",
+      DTOutput("tabla"),
+      width = 30,  
+      height = "810px", 
+      DT::dataTableOutput("table")
+    )
+  )
+)
+
+server <- function(input, output) {
+  datos_filtrados <- eventReactive(input$actualizar, {
+    datos_empleo |>
+      filter(pais_region == input$pais, anyo %in% input$ano_inicio)
+  })
+  
+  output$plot_empleadoras <- renderPlot({
+    genero_variable <- ifelse(input$genero == "Hombres", "empleadores_hombres", "empleadoras_mujeres")
+    ggplot(datos_filtrados(), aes(x = anyo, y = !!sym(genero_variable))) +
+      geom_bar(stat = "identity") +
+      labs(title = paste("Porcentaje de", input$genero, "empleadores en", input$pais),
+           y = "Porcentaje",
+           x = "Año")
+  })
+  
+  output$plot_informal <- renderPlot({
+    genero_variable <- ifelse(input$genero == "Hombres", "empleo_informal_hombres", "empleo_informal_mujeres")
+    ggplot(datos_filtrados(), aes(x = anyo, y = !!sym(genero_variable))) +
+      geom_bar(stat = "identity") +
+      labs(title = paste("Porcentaje de", input$genero, "trabajadores informales en", input$pais),
+           y = "Porcentaje",
+           x = "Año")
+  })
+  
+  output$plot_desempleo <- renderPlot({
+    genero_variable <- ifelse(input$genero == "Hombres", "desempleo_hombres", "desempleo_mujeres")
+    ggplot(datos_filtrados(), aes(x = anyo, y = !!sym(genero_variable))) +
+      geom_bar(stat = "identity") +
+      labs(title = paste("Porcentaje de", input$genero, "desempleo en", input$pais),
+           y = "Porcentaje",
+           x = "Año")
+  })
+  
+  output$tabla <- renderDT({
+    datatable(datos_filtrados(), options = list(pageLength = 100, scrollX = TRUE))
+  })
+}
+
+shinyApp(ui, server)
+
+
